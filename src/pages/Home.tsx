0df54af4f4d0333915d5eb74b5f997599ca5896b
Ed-Games <edmarcosfilho2015@gmail.com>
2021-06-23 22:50:10 -0300

    create: handleJoinRoom function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,12 +7,15 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import '../styles/auth.scss'
 import {Button} from '../components/Button'
 import { useAuth } from '../hooks/useAuth'
+import { FormEvent, useState } from 'react'
+import { database } from '../services/firebase'
 
 
 export function Home(){
 
     const history = useHistory()
     const {user, signInWithGoogle} = useAuth()
+    const [roomCode, setRoomCode] = useState('')
 
     async function handleCreateRoom(){
         if(!user){
@@ -20,6 +23,20 @@ export function Home(){
         }
 
         history.push('/rooms/new')
+    }
+
+    async function handleJoinRoom(event: FormEvent){
+        event.preventDefault()
+        if(roomCode.trim() === '') return
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+        if(!roomRef.exists()){
+            alert('Room does not exist')
+            return
+        }
+
+        history.push(`/rooms/${roomCode}`)
     }
 
     return(
@@ -40,11 +57,13 @@ export function Home(){
                         Ou entre em uma sala
                     </div>
 
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
 
                         <input 
                         type="text"
                         placeholder="Digite o código da sala" 
+                        onChange={event => setRoomCode(event.target.value)}
+                        value={roomCode}
                         />
 
                         <Button type="submit">
