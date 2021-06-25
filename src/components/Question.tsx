0ca427676216000378e40e1ff1c05a88e80a2da0
Ed-Games <edmarcosfilho2015@gmail.com>
2021-06-24 22:24:51 -0300

    update: changing styles if question is answered or highlighted

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 import '../styles/question.scss'
+import cx from 'classnames'
 
 type QuestionProps = {
     content:string,
@@ -7,12 +8,25 @@ type QuestionProps = {
         name:string,
         avatar:string,
     },
-    children?: ReactNode
+    children?: ReactNode,
+    isAnsewered?: boolean,
+    isHightLighted?: boolean, 
 }
 
-export function Question({content, author, children}:QuestionProps){
+export function Question({
+    content, 
+    author, 
+    children,
+    isAnsewered = false,
+    isHightLighted = false,
+    }:QuestionProps){
     return(
-        <div className="question">
+        <div 
+        className={cx(
+            'question',
+            {answered: isAnsewered},
+            {highlighted: isHightLighted && !isAnsewered,}
+        )}>
             <p>{content}</p>
 
             <footer>
